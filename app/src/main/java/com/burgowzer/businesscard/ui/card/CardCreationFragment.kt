0edd6c00166f9ff5b0e3b07204ae9e92a09534f4
Burgowzer <burgowzer@gmail.com>
2020-11-30 02:01:06 +0100

    Navigation animation addition between fragment card and creation fragment

--- app/src/main/java/com/burgowzer/businesscard/ui/card/CardCreationFragment.kt
+++ app/src/main/java/com/burgowzer/businesscard/ui/card/CardCreationFragment.kt
@@ -21,7 +21,7 @@ import kotlinx.android.synthetic.main.fragment_card_creation.*
 class CardCreationFragment: Fragment(), CardManager.CardInformation {
     var realmList: RealmResults<Card>? = null
     val cardViewModel: CardViewModel by viewModels()
-    lateinit var realm: Realm
+    var realm: Realm = Realm.getDefaultInstance()
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
@@ -32,60 +32,6 @@ class CardCreationFragment: Fragment(), CardManager.CardInformation {
 
         return root
     }
-
-
-
-        private fun saveCardInformation(resultHandler: (realmList:RealmResults<Card>?,error:String?)->Unit) {
-
-
-/*
-        firstNameText.text.let{ newCard?.firstName = it.toString()}
-        lastNameText.text.let{newCard?.lastName = it.toString()}
-        jobPositionText.text.let{newCard?.jobPosition = it.toString()}
-        companyNameText.text.let{newCard?.companyName = it.toString()}
-        companyAddressText.text.let{newCard?.companyAddress = it.toString()}
-        phoneNumberText.text.let{newCard?.phoneNumber = it.toString()}
-        emailText.text.let{newCard?.email = it.toString()}*/
-
-
-            val newCard:Card?= Card()
-
-
-            if(firstNameText.text != null){ newCard?.firstName = firstNameText.text.toString()}
-            if(lastNameText.text != null){newCard?.lastName = lastNameText.text.toString()}
-            if(jobPositionText.text != null){newCard?.jobPosition= jobPositionText.text.toString()}
-            if(companyNameText.text != null){newCard?.companyName = companyNameText.text.toString()}
-            if(companyAddressText.text != null){newCard?.companyAddress = companyAddressText.text.toString()}
-            if(phoneNumberText.text != null){newCard?.phoneNumber = phoneNumberText.text.toString()}
-            if(emailText.text != null){ newCard?.email = emailText.text.toString()}
-
-
-
-            realm.beginTransaction()
-            if (newCard != null) {
-                realm.copyToRealm(newCard)
-                realmList = realm.where(Card::class.java).findAll()
-                resultHandler(realmList, null)
-                val creationMessage = Toast.makeText(this, "Card created!", Toast.LENGTH_SHORT)
-                creationMessage.show()
-            }
-            realm.commitTransaction()
-
-
-
-        }
-
-
-        override fun cardInformationFiller(realmList: RealmResults<Card>?) {
-
-        }
-
-        private fun loadCardList (client: CardManager.CardInformation){
-            saveCardInformation { realmList, error->
-                client.cardInformationFiller(realmList)
-            }
-        }
-
 
 
     override fun onResume() {
@@ -103,4 +49,52 @@ class CardCreationFragment: Fragment(), CardManager.CardInformation {
     private fun endCardCreationActivity(){
         findNavController().navigate(R.id.action_cardCreationFragment_to_navigation_cards)
     }
+
+    private fun saveCardInformation(resultHandler: (realmList:RealmResults<Card>?,error:String?)->Unit) {
+
+
+/*
+        firstNameText.text.let{ newCard?.firstName = it.toString()}
+        lastNameText.text.let{newCard?.lastName = it.toString()}
+        jobPositionText.text.let{newCard?.jobPosition = it.toString()}
+        companyNameText.text.let{newCard?.companyName = it.toString()}
+        companyAddressText.text.let{newCard?.companyAddress = it.toString()}
+        phoneNumberText.text.let{newCard?.phoneNumber = it.toString()}
+        emailText.text.let{newCard?.email = it.toString()}*/
+
+
+        val newCard:Card?= Card()
+
+
+        if(firstNameText.text != null){ newCard?.firstName = firstNameText.text.toString()}
+        if(lastNameText.text != null){newCard?.lastName = lastNameText.text.toString()}
+        if(jobPositionText.text != null){newCard?.jobPosition= jobPositionText.text.toString()}
+        if(companyNameText.text != null){newCard?.companyName = companyNameText.text.toString()}
+        if(companyAddressText.text != null){newCard?.companyAddress = companyAddressText.text.toString()}
+        if(phoneNumberText.text != null){newCard?.phoneNumber = phoneNumberText.text.toString()}
+        if(emailText.text != null){ newCard?.email = emailText.text.toString()}
+
+
+        realm.beginTransaction()
+        if (newCard != null) {
+            realm.copyToRealm(newCard)
+            realmList = realm.where(Card::class.java).findAll()
+            resultHandler(realmList, null)
+        }
+        realm.commitTransaction()
+
+
+    }
+
+    private fun loadCardList (client: CardManager.CardInformation){
+        saveCardInformation { realmList, error->
+            client.cardInformationFiller(realmList)
+        }
+    }
+
+    override fun cardInformationFiller(realmList: RealmResults<Card>?) {
+
+    }
 }
+
+
